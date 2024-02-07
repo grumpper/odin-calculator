4b8b3fdcaea2c9df5d5b22b7bd77b341a4c07819
Bozhidar Zhelev <6785016+grumpper@users.noreply.github.com>
2024-02-07 18:11:16 +0200

    create calculation features

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let displayArray = Array()
-let displayNumber = Number()
+let historyArray = []
+let displayArray = []
+let displayNumber = undefined
 const displayCurrent = document.querySelector('.current')
+const displayHistory = document.querySelector('.history')
 
 function hoverOnButtons() {
     const buttons = document.querySelectorAll('button')
@@ -11,9 +13,7 @@ function hoverOnButtons() {
                     e.style.cssText = "color: #EAE2B7; background-color: #003049; font-weight: bold;"
                     break
                 case e.classList.contains('operation'):
-                    e.id === 'equal' ? 
-                    e.style.cssText = "color: #F77F00; background-color: #003049; font-weight: bold;" :
-                    e.style.cssText = "color: #EAE2B7; background-color: #F77F00; font-weight: bold;"
+                    e.id === 'equal' ? e.style.cssText = "color: #F77F00; background-color: #003049; font-weight: bold;" : e.style.cssText = "color: #EAE2B7; background-color: #F77F00; font-weight: bold;"
                     break
                 case e.classList.contains('function'):
                     e.style.cssText = "color: #EAE2B7; background-color: #D62828; font-weight: bold;"
@@ -24,10 +24,26 @@ function hoverOnButtons() {
     })
 }
 
-function generateOperand() {
+function makeCalculations(a, b, action) {
+    switch (action) {
+        case "+":
+            return a + b
+        case "-":
+            return a - b
+        case "x":
+            return a * b
+        case "/":
+            return a / b
+        case "=":
+            return a
+    }
+}
+
+function main() {
     const numberButtons = document.querySelectorAll('.number, .decimal')
     numberButtons.forEach((e) => {
         e.addEventListener('click', () => {
+            // handle operands
             if (e.textContent === '.' && displayArray.includes('.')) {
             } else {
                 displayArray.push(e.textContent)
@@ -39,17 +55,33 @@ function generateOperand() {
             }
         })
     })
-    return displayNumber
-}
 
-function generateOperation() {
     const operationButtons = document.querySelectorAll('.operation')
     operationButtons.forEach((e) => {
         e.addEventListener('click', () => {
-            
+            console.log(historyArray.join(" "))
+            if (displayCurrent.textContent !== "") {
+                historyArray.push(parseFloat(displayCurrent.textContent))
+            } else {
+                historyArray.push("")
+            }
+            console.log(historyArray.join(" "))
+            if (historyArray.length === 3) {
+                let result = makeCalculations(historyArray[0], historyArray[2], historyArray[1])
+                historyArray = [result]
+            }
+            historyArray.push(e.textContent)
+            if (e.textContent === "=") {
+                displayHistory.textContent = ''
+                displayCurrent.textContent = historyArray[0]
+            } else {
+                displayHistory.textContent = historyArray.join(" ")
+                displayCurrent.textContent = ''
+            }
+            displayArray = []
         })
     })
 }
 
 hoverOnButtons()
-generateOperand()
+main()
